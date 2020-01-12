d4d7b6c10e6c17308f29aa8c19dfb8f3ce48fa1d
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-12 22:35:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,13 @@
 class Transfer
  
   # your code here
-    
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
 end
