fcb7650d9d903b25b6969d21132256ea1d5e302d
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 01:35:38 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_accessor :balance, :status
    attr_reader :name
   
-  def initialize(BankAccount)
+  def initialize(:balance, :status)
     @name = name
     @balance = 1000
     @status = open
