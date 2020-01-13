77f8aa392a0ce423881e57f8d196bdd5aa573bd1
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 03:49:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,37 +1,38 @@
 class Transfer
   # your code here
-  attr_accessor :status, :bankaccount
-  attr_reader :amount, :sender, :receiver
+  attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
-              @sender = sender
-              @receiver = receiver
-              @status = "pending"
-              @amount = amount
-            end
+    self.sender = sender
+    self.receiver = receiver
+    self.amount = amount
+    self.status = "pending"
+  end 
 
-            def valid?
-              sender.valid? && receiver.valid?
-            end
-
-            def execute_transaction
-              if sender.balance < amount
-                @status = "rejected"
-                return "Transaction rejected. Please check your account balance."
-              elsif @status != "complete" && sender.valid?
-                @receiver.balance = @receiver.balance + amount
-                @sender.balance = @sender.balance - amount
-                @status = "complete"
-              end
-            end
-
-            def reverse_transfer
-              if self.execute_transaction && valid? && @receiver.balance > amount && status == "complete"
-                #binding.pry
-                @receiver.balance = @receiver.balance - amount
-                @sender.balance = @sender.balance + amount
-                @status = "reversed"
-              end
-            end
-
-          end
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else 
+      false
+    end
+  end
+  
+  def execute_transaction
+    if self.sender.valid? && self.sender.balance > self.amount && self.status != "complete"
+      self.sender.balance -= amount
+      self.receiver.balance += amount
+      self.status = "complete"
+      
+    else self.status = "rejected"
+      puts "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+   if self.status == "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end 
+end
