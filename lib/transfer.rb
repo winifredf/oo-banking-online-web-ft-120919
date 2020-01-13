e3e6dae061dd2b011eafbbb87e0ea33cc449abcf
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 03:35:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,17 +18,14 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-       @sender.balance -= @amount
-       @receiver.balance += @amount
-       @status = "complete"
-    
-    elsif sender.balance < amount
-        @status = "rejected"
-          "Transaction rejected. Please check your account balance."
-      
-    else @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+    if def execute_transaction
+              if sender.balance < amount
+                @status = "rejected"
+                return "Transaction rejected. Please check your account balance."
+              elsif @status != "complete" && sender.valid?
+                @receiver.balance = @receiver.balance + amount
+                @sender.balance = @sender.balance - amount
+                @status = "complete"
     end
   end
   
