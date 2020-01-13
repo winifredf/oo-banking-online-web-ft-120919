9ad3ef96bcb7a0f4752cacaf37721062f1f3ff97
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 03:08:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,9 +23,9 @@ class Transfer
        @receiver.balance += @amount
        @status = "complete"
     
-    elsif @sender.balance = ""
+    
       
-    else @status = "rejected"
+    else @status = "reject"
       "Transaction rejected. Please check your account balance."
     end
   end
